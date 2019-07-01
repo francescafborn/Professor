cbf463fc56280d9672a4b1d269573f043f864676
Administrador <Administrador@10.10.4.28>
2019-07-01 17:21:53 -0300

    PRIMEIRA VERSAO

---
+++ JavaApplication1/src/Modelo/Professor.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Professor {
+    
+     private int codigo;
+     private String nome;
+     private String endereco;
+     private String sobrenome;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" + "codigo=" + codigo + ", nome=" + nome + ", endereco=" + endereco + ", sobrenome=" + sobrenome + '}';
+    }
+          
+}
